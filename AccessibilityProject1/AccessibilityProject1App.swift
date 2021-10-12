02987e6cfac880c0c1d640646e3d3238de972ae9
Abhi <mac@Abhis-MacBook-Air.local>
2021-10-12 10:36:07 +0530

    first commit

---
+++ AccessibilityProject1/AccessibilityProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProject1App.swift
+//  AccessibilityProject1
+//
+//  Created by Abhi on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
